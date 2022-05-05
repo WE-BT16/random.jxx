39c8bce130305c7bbe7172197190f170876c2809
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2022-05-04 22:07:52 -0400

    feat: add types

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare function random(type: "number", min: number, max: number): number;
+declare function random(type: "string", length: number): string;
+declare function random(type: "random"): string;
+export = random;
